9a491dfb8ed6ebf07d9452636b7c4f145f0043b5
simchaphysics@gmail.com <simchaphysics@gmail.com>
2023-12-28 16:53:55 -0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eigenthread.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
