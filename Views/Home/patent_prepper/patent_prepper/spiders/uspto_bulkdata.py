0bc77de1a0d03136c44cdf8f2bd24dbda6328387
simchaphysics@gmail.com <simchaphysics@gmail.com>
2024-01-20 11:12:55 -0800

    Augmented USPTO spider to point at developer.api of USPTO.

--- Views/Home/patent_prepper/patent_prepper/spiders/uspto_bulkdata.py
+++ Views/Home/patent_prepper/patent_prepper/spiders/uspto_bulkdata.py
@@ -1,13 +1,15 @@
 import scrapy
 
-
 class UsptoBulkdataSpider(scrapy.Spider):
     name = "uspto_bulkdata"
-    allowed_domains = ["www.uspto.gov"]
-    #start_urls = ["https://www.uspto.gov"]
+    allowed_domains = ["developer.uspto.gov"]
+    start_urls = ["https://developer.uspto.gov/ibd-api/v1/application/publications?inventionSubjectMatterCategory=utility&publicationFromDate=1994-02-08&publicationToDate=2020-02-08"]
     
     def start_requests(self):
-        yield scrapy.Request(url='https://www.uspto.gov', callback=self.parse, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'})
+        yield scrapy.Request(url='https://developer.uspto.gov/ibd-api/v1/application/publications?inventionSubjectMatterCategory=utility&publicationFromDate=1994-02-08&publicationToDate=2020-02-08', callback=self.parse, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'})
 
     def parse(self, response):
-        yield response.follow(url='https://www.uspto.gov', callback=self.parse, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'})
+        yield response.follow(url='https://developer.uspto.gov/ibd-api/v1/application/publications?inventionSubjectMatterCategory=utility&publicationFromDate=1994-02-08&publicationToDate=2020-02-08', callback=self.parse, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'})
+        
+        print(response.body)
+        
