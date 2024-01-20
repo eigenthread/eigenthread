4c4eb4ef81a7a1ae33dd39ba0bb18a6c3724d5ca
simchaphysics@gmail.com <simchaphysics@gmail.com>
2024-01-20 13:51:00 -0800

    Inserted json library into scraper to select the claimText JSON parameter from the API call for piping output.

--- Views/Home/patent_prepper/patent_prepper/spiders/uspto_bulkdata.py
+++ Views/Home/patent_prepper/patent_prepper/spiders/uspto_bulkdata.py
@@ -1,4 +1,5 @@
 import scrapy
+import json
 
 class UsptoBulkdataSpider(scrapy.Spider):
     name = "uspto_bulkdata"
@@ -9,7 +10,15 @@ class UsptoBulkdataSpider(scrapy.Spider):
         yield scrapy.Request(url='https://developer.uspto.gov/ibd-api/v1/application/publications?inventionSubjectMatterCategory=utility&publicationFromDate=1994-02-08&publicationToDate=2020-02-08', callback=self.parse, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'})
 
     def parse(self, response):
-        yield response.follow(url='https://developer.uspto.gov/ibd-api/v1/application/publications?inventionSubjectMatterCategory=utility&publicationFromDate=1994-02-08&publicationToDate=2020-02-08', callback=self.parse, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'})
+        # yield response.follow(url='https://developer.uspto.gov/ibd-api/v1/application/publications?inventionSubjectMatterCategory=utility&publicationFromDate=1994-02-08&publicationToDate=2020-02-08', callback=self.parse, headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'})
+        # print(response.body)
         
-        print(response.body)
+        json_response = json.loads(response.body)
+        claimText = json_response.get('claimText')
+        #print(claimText)
+
+        #for claim in claimText:
+            #yield {
+                    #'claimText':claim.get('claimText')
+                #}
         
