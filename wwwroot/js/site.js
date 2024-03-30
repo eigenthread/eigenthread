bb75051a782c9eeb35739ba0b397705fba5f9cc6
simchaphysics@gmail.com <simchaphysics@gmail.com>
2024-03-30 13:50:05 -0700

    Inserted sticky navbar.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -16,6 +16,32 @@ btnNavEl.addEventListener('click', function () {
     headerEl.classList.toggle("nav-open");
 });
 
+//////////////////////////////////////////////////////
+// sticky navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+const obs = new IntersectionObserver(
+    function (entries) {
+        const ent = entries[0];
+        console.log(ent);
+
+        if (ent.isIntersecting === false) {
+            document.body.classList.add('sticky');
+        }
+
+        if (ent.isIntersecting) {
+            document.body.classList.remove('sticky');
+        }
+    },
+    {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+});
+obs.observe(sectionHeroEl);
+
+//////////////////////////////////////////////////////
+// fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
     var flex = document.createElement("div");
     flex.style.display = "flex";
