1afc81d9f41ce9eb22f9df43046a06061151229d
simchaphysics@gmail.com <simchaphysics@gmail.com>
2025-01-06 14:48:28 -0800

    Updated basic API into PatentScraper

---
+++ Controllers/USPTOPatentSearchContoller.cs
@@ -0,0 +1,16 @@
+﻿// File: /Controllers/USPTOPatentSearchController.cs
+using Microsoft.AspNetCore.Mvc;
+
+namespace eigenthread.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // Routes to /api/usptopatentsearch
+    public class USPTOPatentSearchController : ControllerBase
+    {
+        [HttpGet("search")] // Routes to /api/usptopatentsearch/search
+        public IActionResult Search()
+        {
+            return Ok(new { Message = "USPTO Patent Search API is working!" });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,50 @@
+/*var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// app.MapBlazorHub();
+// app.MapFallbackToPage("/_Host");
+app.MapControllers(); // Add this line to map controllers
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.UseEndpoints(endpoints =>
+{
+    // Map API controllers
+    endpoints.MapControllers();
+
+    // Fallback to Razor views
+    endpoints.MapFallbackToFile("Views/_Host.cshtml");
+});
+
+app.Run();
+*/
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,7 +56,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,8 +66,14 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+    endpoints.MapFallbackToFile("/_Host.html"); // Fallback to static file
+});
 
 app.Run();
+
